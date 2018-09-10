55a63991a4f1c65759a001e868b4b49132fab651
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-10 14:13:50 +0900

    prettier and format all files

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,10 +2,13 @@ import { connect } from 'react-redux';
 import App from '../components/App';
 import { withRouter } from 'react-router-dom';
 
-const mapStateToProps = state => ({
-});
+const mapStateToProps = state => ({});
 
-const mapDispatchToProps = dispatch => ({
-});
+const mapDispatchToProps = dispatch => ({});
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
