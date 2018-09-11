43a07019140b7972a2658977fd20bdf3a3a07f56
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-11 14:54:32 +0900

    remove withRouter from NotFound component

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
 class NotFound extends Component {
   render() {
@@ -10,9 +9,7 @@ class NotFound extends Component {
 
 const mapStateToProps = state => ({});
 const mapDispatchToProps = dispatch => ({});
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(NotFound)
-);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NotFound);
