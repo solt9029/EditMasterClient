1b8bdc053b9a10194b76c394e650bd212884e1b0
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-11 09:02:02 +0900

    add Dockerfile and docker-compose.yml to build directory!

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
@@ -103,6 +101,18 @@ measureFileSizesBeforeBuild(paths.appBuild)
 // Create the production build and print the deployment instructions.
 function build(previousFileSizes) {
   console.log('Creating an optimized production build...');
+
+  // Copy files to build directory.
+  const copyFiles = ['Dockerfile', 'docker-compose.yml'];
+  copyFiles.forEach(copyFile => {
+    fs.copyFile(copyFile, `./build/${copyFile}`, err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`${copyFile} copied.`);
+    });
+  });
 
   let compiler = webpack(config);
   return new Promise((resolve, reject) => {
