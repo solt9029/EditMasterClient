a9ca1160dd805443694dd1b3e4e99d751dc406d4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-15 20:43:39 +0900

    formReducer added

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,12 +4,14 @@ import * as reducers from './reducers';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { history } from './history';
+import { reducer as formReducer } from 'redux-form';
 
 export default function configureStore() {
   return createStore(
     combineReducers({
       ...reducers,
       router: routerReducer,
+      form: formReducer,
     }),
     applyMiddleware(logger, thunk, routerMiddleware(history))
   );
