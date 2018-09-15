4c09ff72d83003ada3971a48d6290e65ab20e242
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-15 21:07:13 +0900

    style update

--- src/components/parts/Config.js
+++ src/components/parts/Config.js
@@ -4,12 +4,6 @@ import { Form, Input, FormGroup, Label } from 'reactstrap';
 import styled from 'styled-components';
 import { Field } from 'redux-form';
 
-const StyledDiv = styled.div`
-  background-color: #ccc;
-  width: 100%;
-  height: 100%;
-`;
-
 const StyledForm = styled(Form)`
   padding: 15px;
   width: 100%;
@@ -22,60 +16,58 @@ const StyledForm = styled(Form)`
 class Config extends Component {
   render() {
     return (
-      <StyledDiv>
-        <StyledForm>
-          <FormGroup>
-            <Label for="username">ユーザ名</Label>
-            <Input
-              tag={Field}
-              component="input"
-              type="text"
-              name="username"
-              placeholder="ユーザ名（例：通りすがりの創作の達人）"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="videoId">YouTube動画URL</Label>
-            <Input
-              tag={Field}
-              component="input"
-              type="text"
-              name="videoId"
-              placeholder="YouTube動画URL（例：https://www.youtube.com/watch?v=PqJNc9KVIZE）"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="bpm">BPM</Label>
-            <Input
-              tag={Field}
-              component="input"
-              type="number"
-              name="bpm"
-              placeholder="BPM（例：200）"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="offset">OFFSET：曲の始まる時間（秒）</Label>
-            <Input
-              tag={Field}
-              component="input"
-              type="number"
-              name="offset"
-              placeholder="OFFSET（例：1.5）"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="comment">コメント</Label>
-            <Input
-              tag={Field}
-              component="input"
-              type="text"
-              name="comment"
-              placeholder="コメント（例：創作の達人で創作譜面をしました！）"
-            />
-          </FormGroup>
-        </StyledForm>
-      </StyledDiv>
+      <StyledForm>
+        <FormGroup>
+          <Label for="username">ユーザ名</Label>
+          <Input
+            tag={Field}
+            component="input"
+            type="text"
+            name="username"
+            placeholder="ユーザ名（例：通りすがりの創作の達人）"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="videoId">YouTube動画URL</Label>
+          <Input
+            tag={Field}
+            component="input"
+            type="text"
+            name="videoId"
+            placeholder="YouTube動画URL（例：https://www.youtube.com/watch?v=PqJNc9KVIZE）"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="bpm">BPM</Label>
+          <Input
+            tag={Field}
+            component="input"
+            type="number"
+            name="bpm"
+            placeholder="BPM（例：200）"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="offset">OFFSET：曲の始まる時間（秒）</Label>
+          <Input
+            tag={Field}
+            component="input"
+            type="number"
+            name="offset"
+            placeholder="OFFSET（例：1.5）"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="comment">コメント</Label>
+          <Input
+            tag={Field}
+            component="input"
+            type="text"
+            name="comment"
+            placeholder="コメント（例：創作の達人で創作譜面をしました！）"
+          />
+        </FormGroup>
+      </StyledForm>
     );
   }
 }
