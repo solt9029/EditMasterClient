e538145f05b094167f079d0066b33f4068b41a8c
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-15 14:38:06 +0900

    split pane

--- src/components/pages/scores/Show.js
+++ src/components/pages/scores/Show.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Navbar from '../../parts/Navbar';
-import Footer from '../../parts/Footer';
+import SplitterLayout from 'react-splitter-layout';
 
 class Show extends Component {
   render() {
     return (
       <div>
         <Navbar active="scoresNew" />
-        <div>Scores Show {this.props.match.params.id} Page!</div>
-        <Footer />
+        <div>
+          <SplitterLayout vertical percentage secondaryInitialSize={80}>
+            <div>1</div>
+            <SplitterLayout percentage secondaryInitialSize={70}>
+              <div>2</div>
+              <SplitterLayout percentage secondaryInitialSize={43}>
+                <div>3</div>
+                <div>4</div>
+              </SplitterLayout>
+            </SplitterLayout>
+          </SplitterLayout>
+        </div>
       </div>
     );
   }
