e10b8009c819f8b6d338d838de5f32dc71b354d5
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-17 11:36:36 +0900

    functional component

--- src/components/pages/scores/Index.js
+++ src/components/pages/scores/Index.js
@@ -1,26 +1,13 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React from 'react';
 import Navbar from '../../parts/Navbar';
 import Footer from '../../parts/Footer';
 
-class Index extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar active="scoresIndex" form />
-        <div>Scores Index Page!</div>
-        <Footer />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => ({});
-const mapDispatchToProps = dispatch => ({});
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Index)
+const Index = () => (
+  <div>
+    <Navbar active="scoresIndex" form />
+    <div>Scores Index Page!</div>
+    <Footer />
+  </div>
 );
+
+export default Index;
