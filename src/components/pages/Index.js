410324abb56daaf40636d1df1203305b7548d831
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-17 11:34:47 +0900

    functional component

--- src/components/pages/Index.js
+++ src/components/pages/Index.js
@@ -1,27 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React from 'react';
 import Navbar from '../parts/Navbar';
 import Jumbotron from '../parts/Jumbotron';
 import Footer from '../parts/Footer';
 
-class Index extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Jumbotron />
-        <Footer />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => ({});
-const mapDispatchToProps = dispatch => ({});
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Index)
+const Index = () => (
+  <div>
+    <Navbar />
+    <Jumbotron />
+    <Footer />
+  </div>
 );
+
+export default Index;
