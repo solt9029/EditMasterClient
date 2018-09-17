df6fd63d103095b83a69eeaa0b5594d0acf1b2bc
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-17 11:38:33 +0900

    functional component

--- src/components/pages/scores/New.js
+++ src/components/pages/scores/New.js
@@ -1,26 +1,13 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React from 'react';
 import Navbar from '../../parts/Navbar';
 import Footer from '../../parts/Footer';
 
-class New extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar active="scoresNew" />
-        <div>Scores New Page!</div>
-        <Footer />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => ({});
-const mapDispatchToProps = dispatch => ({});
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(New)
+const New = () => (
+  <div>
+    <Navbar active="scoresNew" />
+    <div>Scores New Page!</div>
+    <Footer />
+  </div>
 );
+
+export default New;
