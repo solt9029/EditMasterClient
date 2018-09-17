e64f35a745490141f2806ccaf5545eafcaa7069a
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-17 11:35:41 +0900

    functional component

--- src/components/pages/Help.js
+++ src/components/pages/Help.js
@@ -1,23 +1,13 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import Navbar from '../parts/Navbar';
 import Footer from '../parts/Footer';
 
-class Help extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar active="help" />
-        <div>Help Page!</div>
-        <Footer />
-      </div>
-    );
-  }
-}
+const Help = () => (
+  <div>
+    <Navbar active="help" />
+    <div>Help Page!</div>
+    <Footer />
+  </div>
+);
 
-const mapStateToProps = state => ({});
-const mapDispatchToProps = dispatch => ({});
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Help);
+export default Help;
