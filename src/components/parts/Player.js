71b954ca44ae4ddbdc44c7b5406af34e848df7b9
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-19 21:34:36 +0900

    function

--- src/components/parts/Player.js
+++ src/components/parts/Player.js
@@ -17,7 +17,7 @@ class Player extends Component {
     return secondsPerNote;
   }
 
-  calcNoteIndexesInRange(rangeSecond, secondsPerNote) {
+  calcNoteIndexesInRangeSecond(secondsPerNote, rangeSecond) {
     const initialNoteIndex = Math.ceil(
       (this.props.currentTime - rangeSecond - this.props.config.values.offset) /
         secondsPerNote
@@ -26,6 +26,32 @@ class Player extends Component {
       (this.props.currentTime + rangeSecond - this.props.config.values.offset) /
         secondsPerNote
     );
+    let indexes = [];
+    for (let i = initialNoteIndex; i <= finalNoteIndex; i++) {
+      indexes.push(i);
+    }
+    return indexes;
+  }
+
+  calcNoteIndexesInCanvas(initialNoteX) {
+    // Math.floor(-initialNoteX / size.space.width) is the number of notes that already passed from canvas
+    let initialNoteIndex = Math.floor(-initialNoteX / size.space.width) - 3;
+
+    // the number of notes that canvas can display in it
+    const notesNumber = Math.ceil(
+      (this.props.player.width - 1) / size.space.width
+    );
+
+    let finalNoteIndex = initialNoteIndex + notesNumber + 6;
+
+    // to fix bug drawing
+    if (initialNoteIndex < 0) {
+      initialNoteIndex = 0;
+    }
+    if (finalNoteIndex < 0) {
+      finalNoteIndex = 0;
+    }
+
     let indexes = [];
     for (let i = initialNoteIndex; i <= finalNoteIndex; i++) {
       indexes.push(i);
@@ -42,6 +68,59 @@ class Player extends Component {
     return initialNoteX;
   }
 
+  autoMode(secondsPerNote) {
+    const noteIndexesInGoodJudgeRange = this.calcNoteIndexesInRangeSecond(
+      secondsPerNote,
+      second.range.good
+    );
+
+    // if you use forEach here, you can use return instead of continue, but it doesn't have the same function of break
+    for (let i = 0; i < noteIndexesInGoodJudgeRange.length; i++) {
+      if (
+        noteIndexesInGoodJudgeRange[i] < 0 ||
+        noteIndexesInGoodJudgeRange[i] >= this.props.notes.length
+      ) {
+        continue;
+      }
+      if (
+        this.props.notes[noteIndexesInGoodJudgeRange[i]].state !==
+        id.state.fresh
+      ) {
+        continue;
+      }
+
+      switch (this.props.notes[noteIndexesInGoodJudgeRange[i]].id) {
+        case id.note.don:
+          sound.don.trigger();
+          this.props.setState(noteIndexesInGoodJudgeRange[i], id.state.good);
+          break;
+        case id.note.ka:
+          sound.ka.trigger();
+          this.props.setState(noteIndexesInGoodJudgeRange[i], id.state.good);
+          break;
+        case id.note.bigdon:
+          sound.don.trigger();
+          this.props.setState(noteIndexesInGoodJudgeRange[i], id.state.good);
+          break;
+        case id.note.bigka:
+          sound.ka.trigger();
+          this.props.setState(noteIndexesInGoodJudgeRange[i], id.state.good);
+          break;
+        case id.note.renda:
+          sound.don.trigger();
+          break;
+        case id.note.bigrenda:
+          sound.don.trigger();
+          break;
+        case id.note.balloon:
+          sound.don.trigger();
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
   renderNotes() {
     if (!this.props.config) {
       return;
@@ -51,70 +130,32 @@ class Player extends Component {
     }
 
     const secondsPerNote = this.calcSecondsPerNote();
-
-    // x position of initial note
-    const initialNoteX = this.calcInitialNoteX(secondsPerNote);
-
-    // Math.floor(-initialNoteX / size.space.width) is the number of notes that already passed from canvas
-    let canvasInitialNoteIndex =
-      Math.floor(-initialNoteX / size.space.width) - 3;
-    // the number of notes that canvas can display in it
-    const canvasNotesNumber = Math.ceil(
-      (this.props.player.width - 1) / size.space.width
-    );
-    let canvasFinalNoteIndex = canvasInitialNoteIndex + canvasNotesNumber + 6;
-    if (canvasInitialNoteIndex < 0) {
-      canvasInitialNoteIndex = 0;
-    }
-    if (canvasFinalNoteIndex < 0) {
-      canvasInitialNoteIndex = 0;
-    }
+    const initialNoteX = this.calcInitialNoteX(secondsPerNote); // x position of initial note
+    const noteIndexesInCanvas = this.calcNoteIndexesInCanvas(initialNoteX);
     const slicedNotes = this.props.notes.slice(
-      canvasInitialNoteIndex,
-      canvasFinalNoteIndex
+      noteIndexesInCanvas[0],
+      noteIndexesInCanvas[noteIndexesInCanvas.length - 1]
     );
     const reversedSlicedNotes = slicedNotes.reverse();
 
     // sample code sound (not good)
     if (this.props.isAutoMode) {
-      const noteIndexesInGoodJudgeRange = this.calcNoteIndexesInRange(
-        second.range.good,
-        secondsPerNote
-      );
-
-      // if you use forEach here, you can use return instead of continue, but it doesn't have the same function of break
-      for (let i = 0; i < noteIndexesInGoodJudgeRange.length; i++) {
-        if (
-          noteIndexesInGoodJudgeRange[i] < 0 ||
-          noteIndexesInGoodJudgeRange[i] >= this.props.notes.length
-        ) {
-          continue;
-        }
-        if (
-          this.props.notes[noteIndexesInGoodJudgeRange[i]].id === id.note.don &&
-          this.props.notes[noteIndexesInGoodJudgeRange[i]].state ===
-            id.state.fresh
-        ) {
-          sound.don.trigger();
-          this.props.setState(noteIndexesInGoodJudgeRange[i], id.state.good);
-          break;
-        }
-      }
+      this.autoMode(secondsPerNote);
     }
 
     return reversedSlicedNotes.map((note, index) => {
       const x =
         initialNoteX +
-        (canvasInitialNoteIndex + (reversedSlicedNotes.length - index)) *
+        (noteIndexesInCanvas[0] + (reversedSlicedNotes.length - index)) *
           size.space.width;
-
       const y = (this.props.player.height - 1) / 2;
-      const previous =
+      const previousNoteId =
         index < reversedSlicedNotes.length - 1
           ? reversedSlicedNotes[index + 1].id
           : id.note.space;
-      const next =
+      const nextNoteId =
         index > 0 ? reversedSlicedNotes[index - 1].id : id.note.space;
+
       switch (note.id) {
         case id.note.don:
           return <NoteCircle x={x} y={y} size="normal" color="red" />;
@@ -125,8 +166,8 @@ class Player extends Component {
         case id.note.bigka:
           return <NoteCircle x={x} y={y} size="big" color="blue" />;
         case id.note.renda:
-          if (previous === id.note.renda) {
-            if (next === id.note.renda) {
+          if (previousNoteId === id.note.renda) {
+            if (nextNoteId === id.note.renda) {
               return <NoteExtension size="normal" x={x} y={y} color="yellow" />;
             }
             return <NoteEnd x={x} y={y} size="normal" color="yellow" />;
@@ -134,8 +175,8 @@ class Player extends Component {
             return <NoteCircle x={x} y={y} size="normal" color="yellow" />;
           }
         case id.note.bigrenda:
-          if (previous === id.note.bigrenda) {
-            if (next === id.note.bigrenda) {
+          if (previousNoteId === id.note.bigrenda) {
+            if (nextNoteId === id.note.bigrenda) {
               return <NoteExtension size="big" x={x} y={y} color="yellow" />;
             }
             return <NoteEnd x={x} y={y} size="big" color="yellow" />;
@@ -143,8 +184,8 @@ class Player extends Component {
             return <NoteCircle x={x} y={y} size="big" color="yellow" />;
           }
         case id.note.balloon:
-          if (previous === id.note.balloon) {
-            if (next === id.note.balloon) {
+          if (previousNoteId === id.note.balloon) {
+            if (nextNoteId === id.note.balloon) {
               return <NoteExtension size="normal" x={x} y={y} color="red" />;
             }
             return <NoteEnd x={x} y={y} size="normal" color="red" />;
