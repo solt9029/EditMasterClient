595e8f0080a2b0e140852ec2076f4b77859d4edf
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-22 18:19:35 +0900

    editor

--- src/components/parts/Editor.js
+++ src/components/parts/Editor.js
@@ -1,14 +1,9 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Stage, Layer } from 'react-konva';
 import { number, size, position, percentage, id } from '../../constants';
-import Bars from './Bars';
 import { throttle } from 'lodash';
 import { setMousePosition } from '../../actions/editor';
-import Caret from './Caret';
-import NoteCircle from './NoteCircle';
-import NoteExtension from './NoteExtension';
-import NoteEnd from './NoteEnd';
+import Canvas from '../../Canvas';
 
 class Editor extends Component {
   constructor(props) {
@@ -16,6 +11,48 @@ class Editor extends Component {
     this.setMousePosition = throttle(event => {
       this.props.setMousePosition(event.evt.offsetX, event.evt.offsetY);
     }, 100).bind(this);
+    this.canvasRef = React.createRef();
+    this.canvas = null;
+  }
+
+  componentDidMount() {
+    const ctx = this.canvasRef.current.getContext('2d');
+    this.canvas = new Canvas(ctx);
+    this.updateCanvas();
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    if (!this.props.palette) {
+      return;
+    }
+    const barWidth =
+      this.props.editorPane.width - 1 - position.editor.bar.x * 2;
+    const actualBarWidth = barWidth * (1 - percentage.editor.barStartLine); // left side of initial beat line is not available
+    const spaceWidth = actualBarWidth / number.score.column;
+    const barStartLineX =
+      position.editor.bar.x + barWidth * percentage.editor.barStartLine;
+
+    for (let i = this.props.notes.length - 1; i >= 0; i--) {
+      const note = this.props.notes[i];
+      if (note.id === id.note.space) {
+        continue;
+      }
+      const c = i % number.score.column;
+      const l = Math.floor(i / number.score.column);
+      const x = barStartLineX + spaceWidth * c;
+      const y = size.editor.bar.outside.height * (l + 0.5);
+      const previousNoteId = i > 0 ? this.props.notes[i - 1].id : id.note.space;
+      const nextNoteId =
+        i < this.props.notes.length - 1
+          ? this.props.notes[i + 1].id
+          : id.note.space;
+
+      this.canvas.drawNote(x, y, 'player', note.id, previousNoteId, nextNoteId);
+    }
   }
 
   calcMouseNoteIndex(barWidth, barStartLineX) {
@@ -33,93 +70,15 @@ class Editor extends Component {
     return mouseNoteIndex;
   }
 
-  renderNotes() {
-    if (!this.props.palette) {
-      return;
-    }
-
-    const barWidth =
-      this.props.editorPane.width - 1 - position.editor.bar.x * 2;
-    const actualBarWidth = barWidth * (1 - percentage.editor.barStartLine); // left side of initial beat line is not available
-    const spaceWidth = actualBarWidth / number.score.column;
-    const barStartLineX =
-      position.editor.bar.x + barWidth * percentage.editor.barStartLine;
-
-    let notes = [];
-    for (let i = this.props.notes.length - 1; i >= 0; i--) {
-      const note = this.props.notes[i];
-      if (note.id === id.note.space) {
-        continue;
-      }
-      const c = i % number.score.column;
-      const l = Math.floor(i / number.score.column);
-      const x = barStartLineX + spaceWidth * c;
-      const y = size.editor.bar.outside.height * (l + 0.5);
-      const previousNoteId = i > 0 ? this.props.notes[i - 1].id : id.note.space;
-      const nextNoteId =
-        i < this.props.notes.length - 1
-          ? this.props.notes[i + 1].id
-          : id.note.space;
-      if (
-        note.id === id.note.renda ||
-        note.id === id.note.bigrenda ||
-        note.id === id.note.balloon
-      ) {
-        if (previousNoteId === note.id) {
-          if (nextNoteId === note.id) {
-            notes.push(
-              <NoteExtension
-                key={i}
-                pane="editor"
-                spaceWidth={spaceWidth}
-                x={x}
-                y={y}
-                id={note.id}
-              />
-            );
-            continue;
-          }
-          notes.push(
-            <NoteEnd key={i} pane="editor" x={x} y={y} id={note.id} />
-          );
-          continue;
-        }
-      }
-      notes.push(<NoteCircle key={i} pane="editor" x={x} y={y} id={note.id} />);
-    }
-    return notes;
-  }
-
   render() {
-    if (!this.props.palette || this.props.palette) {
-      return <Fragment />;
-    }
-
-    const barNum = Math.ceil(this.props.notes.length / number.score.column);
-    const barWidth =
-      this.props.editorPane.width - 1 - position.editor.bar.x * 2;
-    const actualBarWidth = barWidth * (1 - percentage.editor.barStartLine); // left side of initial beat line is not available
-    const spaceWidth = actualBarWidth / number.score.column;
-    const barStartLineX =
-      position.editor.bar.x + barWidth * percentage.editor.barStartLine;
-    const mouseBarIndex = Math.floor(
-      this.props.mousePosition.y / size.editor.bar.outside.height
-    );
-    let mouseNoteIndex = this.calcMouseNoteIndex(barWidth, barStartLineX);
-
     return (
       <div>
-        <Stage
-          onMouseMove={this.setMousePosition}
+        <canvas
+          ref={this.canvasRef}
+          style={{ display: 'block' }}
           width={this.props.editorPane.width - 1}
-          height={barNum * size.editor.bar.outside.height}
-        >
-          <Layer>
-            <Bars />
-            <Caret />
-            {this.renderNotes()}
-          </Layer>
-        </Stage>
+          height={this.props.editorPane.height - 1}
+        />
       </div>
     );
   }
