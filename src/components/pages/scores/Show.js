cbe148bedf76ed8c5b7f908e9c1be105657e058a
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-26 21:09:39 +0900

    class component

--- src/components/pages/scores/Show.js
+++ src/components/pages/scores/Show.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from '../../parts/Navbar';
 import IDE from '../../parts/IDE';
 
-const Show = () => (
-  <div>
-    <Navbar active="scoresNew" />
-    <IDE />
-  </div>
-);
-
-export default Show;
+export default class Show extends Component {
+  componentDidMount() {
+    // axios here
+  }
+  render() {
+    return (
+      <div>
+        <Navbar active="scoresNew" />
+        <IDE />
+      </div>
+    );
+  }
+}
