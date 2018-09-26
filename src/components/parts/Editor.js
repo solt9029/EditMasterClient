0f9a827c6d33b32b9f70e1f7ddbca3ab3d4fe516
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-26 19:24:03 +0900

    remove throttle

--- src/components/parts/Editor.js
+++ src/components/parts/Editor.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { number, size, position, percentage, id } from '../../constants';
-import { throttle } from 'lodash';
 import Canvas from '../../Canvas';
 import EditorCaretCanvas from './EditorCaretCanvas';
 import EditorCurrentTimeCanvas from './EditorCurrentTimeCanvas';
