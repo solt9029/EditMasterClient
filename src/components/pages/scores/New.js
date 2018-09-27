cf149d5940e7394b0af037d15c24f5a199c050e2
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-28 01:07:22 +0900

    reset

--- src/components/pages/scores/New.js
+++ src/components/pages/scores/New.js
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from '../../parts/Navbar';
 import Footer from '../../parts/Footer';
 import IDE from '../../parts/IDE';
+import { defaultState as editorDefaultState } from '../../../reducers/editor';
+import { defaultState as configDefaultState } from '../../../reducers/config';
+import { connect } from 'react-redux';
+import { setConfig } from '../../../actions/config';
+import { change } from 'redux-form';
+import { calcSecondsPerNote, replaceStates } from '../../../actions/player';
+import { replaceNoteIds } from '../../../actions/editor';
+import constants from '../../../constants';
 
-const New = () => (
-  <div>
-    <Navbar />
-    <IDE />
-    <Footer />
-  </div>
-);
+class New extends Component {
+  componentDidMount() {
+    this.props.replaceNoteIds(editorDefaultState.noteIds);
+    let states = Array(editorDefaultState.noteIds.length);
+    states.fill(constants.id.note.space);
+    this.props.replaceStates(states);
 
-export default New;
+    // need check of configForm???
+    this.props.change('username', configDefaultState.username);
+    this.props.change('videoId', configDefaultState.videoId);
+    this.props.change('offset', configDefaultState.offset);
+    this.props.change('comment', configDefaultState.comment);
+    this.props.change('speed', configDefaultState.speed);
+    this.props.change('bpm', configDefaultState.bpm);
+    this.props.calcSecondsPerNote(configDefaultState.bpm);
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <IDE />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  configForm: state.form.config,
+});
+const mapDispatchToProps = dispatch => ({
+  setConfigInitialValues(config) {
+    dispatch(setConfig(config));
+  },
+  change(field, value) {
+    dispatch(change('config', field, value));
+  },
+  calcSecondsPerNote(bpm) {
+    dispatch(calcSecondsPerNote(bpm));
+  },
+  replaceNoteIds(noteIds) {
+    dispatch(replaceNoteIds(noteIds));
+  },
+  replaceStates(states) {
+    dispatch(replaceStates(states));
+  },
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(New);
--- src/reducers/config.js
+++ src/reducers/config.js
@@ -1,10 +1,19 @@
-const initialState = {
+export const defaultState = {
   username: '通りすがりの創作の達人',
   videoId: 'PqJNc9KVIZE',
   bpm: 150,
   offset: 12.08,
   comment: '創作の達人で創作譜面をしました！',
   speed: 1,
+};
+
+const initialState = {
+  username: '',
+  videoId: '',
+  bpm: 0,
+  offset: 0,
+  comment: '',
+  speed: 0,
 };
 
 export default (state = initialState, action) => {
