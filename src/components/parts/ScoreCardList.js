7f2adad69834aefc075ca970b4670414f7e74e53
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-27 22:59:13 +0900

    score card list almost done!

--- src/components/parts/ScoreCardList.js
+++ src/components/parts/ScoreCardList.js
@@ -1,14 +1,24 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ScoreCard from './ScoreCard';
+import { Container, Row } from 'reactstrap';
+import styled from 'styled-components';
+
+const StyledContainer = styled(Container)`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
 
 class ScoreCardList extends Component {
   render() {
     return (
-      <Fragment>
-        {this.props.data.map((score, i) => {
-          return <div key={i}>{score.speed} </div>;
-        })}
-      </Fragment>
+      <StyledContainer>
+        <Row>
+          {this.props.data.map((score, i) => {
+            return <ScoreCard score={score} key={i} />;
+          })}
+        </Row>
+      </StyledContainer>
     );
   }
 }
