9a24d0309e1d7dce0aa5306c5b6829d723a4f3e8
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-27 18:46:28 +0900

    navbar create & not found

--- src/components/parts/Navbar.js
+++ src/components/parts/Navbar.js
@@ -16,6 +16,9 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import withRouter from 'react-router-dom/withRouter';
 import constants from '../../constants';
+import axios from 'axios';
+import config from '../../config';
+import { connect } from 'react-redux';
 
 const Logo = styled(NavbarBrand)`
   background: url('/images/icon.png') no-repeat left center;
@@ -43,12 +46,40 @@ class Navbar extends Component {
       isOpen: false,
     };
     this.toggle = this.toggle.bind(this);
+    this.create = this.create.bind(this);
   }
 
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen,
     });
+  }
+
+  // this method should be available after config form is mounted
+  // this doesn't need to be used before config form is mounted
+  async create() {
+    if (!this.props.configForm) {
+      return;
+    }
+
+    const data = {
+      bpm: this.props.configForm.values.bpm,
+      videoId: this.props.configForm.values.videoId,
+      username: this.props.configForm.values.username,
+      offset: this.props.configForm.values.offset,
+      speed: this.props.configForm.values.speed,
+      comment: this.props.configForm.values.comment,
+      noteIds: this.props.noteIds,
+    };
+
+    try {
+      axios.post(
+        `http://${config.api.host}:${config.api.port}/scores/create`,
+        data
+      );
+    } catch (error) {
+      // console.log(error.response);
+    }
   }
 
   render() {
@@ -105,6 +136,17 @@ class Navbar extends Component {
                 </StyledNavLink>
               </NavItem>
             </Nav>
+            {(this.props.match.path === constants.route.scores.new ||
+              this.props.match.path === constants.route.scores.show) &&
+              !this.props.notFound && (
+                <Button
+                  color="success"
+                  className="my-2 my-sm-2"
+                  onClick={this.create}
+                >
+                  保存
+                </Button>
+              )}
             {this.props.match.path === constants.route.scores.index && (
               <Form inline onSubmit={e => e.preventDefault()}>
                 <Input type="search" className="mr-sm-2" placeholder="検索" />
@@ -125,4 +167,18 @@ class Navbar extends Component {
   }
 }
 
-export default withRouter(Navbar);
+const mapStateToProps = state => ({
+  configForm: state.form.config,
+  noteIds: state.editor.noteIds,
+});
+const mapDispatchToProps = dispatch => ({
+  // setState(index, state) {
+  //   dispatch(setState(index, state));
+  // },
+});
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Navbar)
+);
