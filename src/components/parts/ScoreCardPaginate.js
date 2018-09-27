786ac3f55093ef5803cfeaa7301ca56d526b573b
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-27 22:09:02 +0900

    del fragment

--- src/components/parts/ScoreCardPaginate.js
+++ src/components/parts/ScoreCardPaginate.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ReactPaginate from 'react-paginate';
 import { fetchData } from '../../actions/score';
