d19134d1c4a1bee997ec1c3a2586fe98d9a883d2
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-27 14:15:51 +0900

    IDE

--- src/components/pages/scores/New.js
+++ src/components/pages/scores/New.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../../parts/Navbar';
 import Footer from '../../parts/Footer';
+import IDE from '../../parts/IDE';
 
 const New = () => (
   <div>
     <Navbar />
-    <div>Scores New Page!</div>
+    <IDE />
     <Footer />
   </div>
 );
