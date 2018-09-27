d2ae6c558034c64905a2251a836b858e6970dff6
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-27 20:07:21 +0900

    simple scores component added

---
+++ src/components/parts/Scores.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Scores extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  async componentDidMount() {
+    const result = await axios.get('http://localhost:8051/scores');
+    console.log(result);
+  }
+
+  render() {
+    return <div />;
+  }
+}
