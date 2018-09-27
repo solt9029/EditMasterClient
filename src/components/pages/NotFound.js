90486e90bb6634a114c5bacf5405eb32ea7d98db
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-27 13:51:40 +0900

    not found

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../parts/Navbar';
+import { Container } from 'reactstrap';
+import Footer from '../parts/Footer';
 
-const NotFound = () => <div>NotFound Page!</div>;
+const Icon = styled.img`
+  width: 25%;
+  min-width: 150px;
+  margin-top: 20px;
+  opacity: 0.5;
+`;
+
+const StyledContainer = styled(Container)`
+  text-align: center;
+`;
+
+const StyledText = styled.p`
+  font-size: 1.2em;
+`;
+
+const NotFound = () => (
+  <div>
+    <Navbar notFound />
+    <StyledContainer>
+      <Icon src="/images/icon.png" />
+      <StyledText>お探しのページは存在しません</StyledText>
+    </StyledContainer>
+    <Footer />
+  </div>
+);
 
 export default NotFound;
