8cbfe29a4e4267de0b9b185cc9e9f44f6a6842b1
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-27 20:37:40 +0900

    score card

--- src/components/parts/ScoreCardList.js
+++ src/components/parts/ScoreCardList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-export default class Scores extends Component {
+export default class ScoreCardList extends Component {
   constructor(props) {
     super(props);
   }
