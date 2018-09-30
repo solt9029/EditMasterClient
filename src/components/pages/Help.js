731bcd1a16fbb9a8f10b8f2def5032b8a74b53c4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-30 14:31:05 +0900

    help

--- src/components/pages/Help.js
+++ src/components/pages/Help.js
@@ -18,13 +18,55 @@ const Help = () => (
     <Navbar />
     <StyledContainer>
       <StyledCard>
+        <CardHeader>どうやって創作譜面をするの？</CardHeader>
+        <CardBlock>
+          <CardText>
+            ●
+            右側の画面のドンやカッを選択した状態で、中央の画面をクリックするとノーツが配置できます。
+          </CardText>
+          <CardText>
+            ●
+            または、中央の画面で数字をタイピングすることでノーツを配置することもできます。こちらの手法ですと、素早く創作譜面ができるのでお勧めです。1がドン、2がカッ、3が大ドン、4が大カッ、5が連打、6が大連打、7が風船、0が空白、といったように対応しています。
+          </CardText>
+        </CardBlock>
+      </StyledCard>
+
+      <StyledCard>
         <CardHeader>どうやって再生するの？</CardHeader>
         <CardBlock>
           <CardText>
-            ・YouTubeの動画IDとBPMとOFFSETがフォームに入力されていることを確認してください。
+            ●
+            YouTubeの動画IDとBPMとOFFSETがフォームに入力されていることを確認してください。
           </CardText>
           <CardText>
-            ・YouTubeの動画を再生すると、それに合わせて譜面が再生されます。
+            ● YouTubeの動画を再生すると、それに合わせて譜面が再生されます。
+          </CardText>
+        </CardBlock>
+      </StyledCard>
+
+      <StyledCard>
+        <CardHeader>プレイモードではどうやってプレイするの？</CardHeader>
+        <CardBlock>
+          <CardText>
+            ●
+            上側の画面をクリックしてください。その状態で、FまたはJのキーを入力するとドン、DまたはKのキーを入力するとカッになります。
+          </CardText>
+        </CardBlock>
+      </StyledCard>
+
+      <StyledCard>
+        <CardHeader>他に分からないことがあるんだけど！</CardHeader>
+        <CardBlock>
+          <CardText>
+            ● 何か分からない点や、追加して欲しい機能などがありましたら、
+            <a
+              href="https://twitter.com/solt9029"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @solt9029
+            </a>
+            までご連絡いただけると幸いです。よろしくお願いします。
           </CardText>
         </CardBlock>
       </StyledCard>
