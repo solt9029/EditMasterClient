a2674adc520c8bb925c9897cf55fc18ddfbe911c
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-30 11:22:07 +0900

    description

---
+++ src/components/parts/News.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Card, CardHeader, CardBlock, CardText, Container } from 'reactstrap';
+import styled from 'styled-components';
+
+const StyledContainer = styled(Container)`
+  margin-top: 100px;
+`;
+
+const StyledCard = styled(Card)`
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
+
+const News = () => (
+  <StyledContainer>
+    <StyledCard>
+      <CardHeader>
+        更新情報：創作の達人を大幅にアップデートしました（2018/09/30）
+      </CardHeader>
+      <CardBlock>
+        <CardText>
+          創作画面にYouTube動画が表示されるようになりました。プレイモードが追加されました。倍速設定ができるようになりました。また、創作譜面を検索できるようになりました。その他にも細かなデザイン修正などが行われています。是非お楽しみください。また、システムの不具合やバグなどを見つけた場合には、
+          <a
+            href="https://twitter.com/solt9029"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @solt9029
+          </a>
+          までご連絡ください。
+        </CardText>
+      </CardBlock>
+    </StyledCard>
+  </StyledContainer>
+);
+
+export default News;
