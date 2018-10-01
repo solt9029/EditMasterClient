32e820fe1a9505e63f3bcbcf60260c7799aadf4e
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-10-01 17:16:49 +0900

    Updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http');
 http
   .createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    var output = fs.readFileSync('/index.html', 'utf-8');
+    var output = fs.readFileSync('./index.html', 'utf-8');
     res.end(output);
   })
   .listen(80);
