c80f50a08c47f7efba70dedea7aa46da87b73ae5
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-10-02 12:16:44 +0900

    redirect

--- src/components/App.js
+++ src/components/App.js
@@ -5,52 +5,15 @@ import ScoresNew from './pages/scores/New';
 import ScoresShow from './pages/scores/Show';
 import Help from './pages/Help';
 import NotFound from './pages/NotFound';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
 import constants from '../constants';
-import qs from 'qs';
-
-const ToIndex = () => {
-  return <Redirect to={constants.route.index} />;
-};
-
-const ToHelp = () => {
-  return <Redirect to={constants.route.help} />;
-};
-
-const ToScoresIndex = () => {
-  return <Redirect to={constants.route.scores.index} />;
-};
-
-class ToScoresNewOrScoresShow extends Component {
-  render() {
-    const query = qs.parse(this.props.location.search, {
-      ignoreQueryPrefix: true,
-    });
-    if (query.id) {
-      const to = `/scores/${query.id}`; // show
-      return <Redirect to={to} />;
-    }
-    return <Redirect to={constants.route.scores.new} />;
-  }
-}
-ToScoresNewOrScoresShow = withRouter(ToScoresNewOrScoresShow);
 
 class App extends Component {
   render() {
     return (
       <div>
         <Switch>
-          {/* redirect for the previous system */}
-          <Route exact path="/Scores/index" component={ToIndex} />
-          <Route exact path="/Scores/help" component={ToHelp} />
-          <Route exact path="/Scores/view" component={ToScoresIndex} />
-          <Route
-            exact
-            path="/Scores/edit"
-            component={ToScoresNewOrScoresShow}
-          />
-
           <Route exact path={constants.route.index} component={Index} />
           <Route
             exact
