08368803f5c28d8e85b58e548d88fee25dd7bdcd
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-10-02 12:16:44 +0900

    redirect

--- docker/server.js
+++ docker/server.js
@@ -3,6 +3,26 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname));
+
+app.get('/Scores/index', (req, res) => {
+  res.redirect('/');
+});
+
+app.get('/Scores/help', (req, res) => {
+  res.redirect('/help');
+});
+
+app.get('/Scores/view', (req, res) => {
+  res.redirect('/scores');
+});
+
+app.get('/Scores/edit', (req, res) => {
+  if (req.query.id) {
+    res.redirect(`/scores/${req.query.id}`);
+  }
+  res.redirect('/scores/new');
+});
+
 app
   .get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
   .listen(80);
