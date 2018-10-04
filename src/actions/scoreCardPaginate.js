4cd1ea71d075f03ed492bb8ba689bd29479966f8
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-10-04 17:25:15 +0900

    score card list

--- src/actions/scoreCardPaginate.js
+++ src/actions/scoreCardPaginate.js
@@ -1,18 +1,36 @@
 import axios from 'axios';
 import config from '../config';
 
-export const fetchData = (page, keyword) => {
+export const fetch = (page, keyword) => {
   return async dispatch => {
-    const result = await axios.get(
-      `http://${config.api.host}:${
-        config.api.port
-      }/scores?page=${page}&keyword=${keyword}`
-    );
-    dispatch(setData(result.data));
+    dispatch(startRequest());
+    try {
+      const result = await axios.get(
+        `http://${config.api.host}:${
+          config.api.port
+        }/scores?page=${page}&keyword=${keyword}`
+      );
+      dispatch(finishRequestSuccess(result.data));
+    } catch (error) {
+      dispatch(finishRequestError(error));
+    }
   };
 };
 
-export const setData = data => ({
-  type: 'SET_DATA',
-  payload: data,
+export const startRequest = () => ({
+  type: 'SCORE_CARD_PAGINATE/START_REQUEST',
+});
+
+export const finishRequestSuccess = data => ({
+  type: 'SCORE_CARD_PAGINATE/FINISH_REQUEST_SUCCESS',
+  payload: { data },
+});
+
+export const finishRequestError = error => ({
+  type: 'SCORE_CARD_PAGINATE/FINISH_REQUEST_ERROR',
+  payload: { error },
+});
+
+export const reset = () => ({
+  type: 'SCORE_CARD_PAGINATE/RESET',
 });
--- src/reducers/scoreCardPaginate.js
+++ src/reducers/scoreCardPaginate.js
@@ -6,21 +6,49 @@ const initialState = {
   perPage: 0,
   total: 0,
   to: 0,
+  isLoading: false,
+  error: null,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'SET_DATA':
+    case 'SCORE_CARD_PAGINATE/START_REQUEST':
       return {
         ...state,
-        data: action.payload.data,
-        currentPage: action.payload.current_page,
-        from: action.payload.from,
-        lastPage: action.payload.last_page,
-        perPage: action.payload.per_page,
-        total: action.payload.total,
-        to: action.payload.to,
+        isLoading: true,
+        error: null,
       };
+    case 'SCORE_CARD_PAGINATE/FINISH_REQUEST_SUCCESS':
+      const {
+        data,
+        current_page,
+        from,
+        last_page,
+        per_page,
+        total,
+        to,
+      } = action.payload.data;
+      return {
+        ...state,
+        data,
+        currentPage: current_page,
+        from,
+        lastPage: last_page,
+        perPage: per_page,
+        total,
+        to,
+        isLoading: false,
+        error: null,
+      };
+    case 'SCORE_CARD_PAGINATE/FINISH_REQUEST_ERROR':
+      const { error } = action.payload;
+      return {
+        ...state,
+        isLoading: false,
+        error,
+      };
+    case 'SCORE_CARD_PAGINATE/RESET':
+      return initialState;
     default:
       return state;
   }
