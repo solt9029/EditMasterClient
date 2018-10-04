1d785fb154d8326d7bb0b66d5c295f4438e157a6
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-10-04 20:02:16 +0900

    create

--- src/actions/modal.js
+++ src/actions/modal.js
@@ -1,6 +1,39 @@
+import axios from 'axios';
+import config from '../config';
+
 export const startCreate = () => ({
   type: 'MODAL/START_CREATE',
 });
+
+export const create = () => {
+  return async (dispatch, getState) => {
+    dispatch(startCreate());
+
+    const state = getState();
+    const { bpm, videoId, username, offset, speed, comment } = state.config;
+    const { notes } = state.editor;
+
+    const data = {
+      bpm: bpm.value,
+      video_id: videoId.value,
+      username: username.value,
+      offset: offset.value,
+      speed: speed.value,
+      comment: comment.value,
+      notes,
+    };
+
+    try {
+      const result = await axios.post(
+        `http://${config.api.host}:${config.api.port}/scores/create`,
+        data
+      );
+      dispatch(finishCreateSuccess(result.data.id));
+    } catch (error) {
+      dispatch(finishCreateError(error.response.data.errors));
+    }
+  };
+};
 
 export const finishCreateSuccess = id => ({
   type: 'MODAL/FINISH_CREATE_SUCCESS',
