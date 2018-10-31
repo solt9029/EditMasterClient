8c7363ee07a5533bd985d3ec19fb69729410da67
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-10-31 21:42:24 +0900

    ogp

--- docker/server.js
+++ docker/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const request = require('request');
 const app = express();
 
+// view settings
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'ogp'));
+
 // redirect
 app.get('/Scores/index', (req, res) => {
   res.redirect('/');
@@ -26,48 +30,48 @@ app.get('/Scores/edit', (req, res) => {
 // serve
 app.get('/', (req, res) => {
   if (req.headers['user-agent'].startsWith('Twitterbot')) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(metas.index);
+    res.sendFile(path.join(__dirname, 'ogp', 'index.html'));
+    return;
   }
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/help', (req, res) => {
   if (req.headers['user-agent'].startsWith('Twitterbot')) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(metas.help);
+    res.sendFile(path.join(__dirname, 'ogp', 'help.html'));
+    return;
   }
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/scores', (req, res) => {
   if (req.headers['user-agent'].startsWith('Twitterbot')) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(metas.scores.index);
+    res.sendFile(path.join(__dirname, 'ogp', 'scores', 'index.html'));
+    return;
   }
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/scores/new', (req, res) => {
   if (req.headers['user-agent'].startsWith('Twitterbot')) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(metas.scores.new);
+    res.sendFile(path.join(__dirname, 'ogp', 'scores', 'new.html'));
+    return;
   }
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/scores/:id', (req, res) => {
   if (req.headers['user-agent'].startsWith('Twitterbot')) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
     request.get(
       {
-        url: `http://${config.api.HOST}:${config.api.PORT}/scores/${
-          req.params.id
-        }`,
+        url: `http://editmasterapi.solt9029.com/scores/${req.params.id}`,
       },
       (error, response, body) => {
         const score = JSON.parse(body);
-        res.end(metas.scores.show(score));
+        if (score.comment === null) {
+          score.comment = '';
+        }
+        res.render('scores/show', { score: score });
       }
     );
     return;
@@ -81,86 +85,5 @@ app.use(express.static(__dirname));
 // all
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
+// listen
 app.listen(80);
-
-const config = {
-  api: {
-    HOST: 'editmasterapi.solt9029.com',
-    PORT: '80',
-  },
-};
-
-const metas = {
-  index: `
-  <html>
-  <head>
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content="創作の達人">
-  <meta name="twitter:description" content="誰もが知っている太鼓ゲーム。実は譜面を叩くだけでなく、創作譜面も面白い。創作の達人でオリジナルの譜面を創作しよう。誰でも簡単に始められる、創作譜面支援アプリ。">
-  <meta name="twitter:image" content="http://editmaster.solt9029.com/images/twittercard.png">
-  <meta name="twitter:url" content="http://editmaster.solt9029.com">
-  <meta name="twitter:site" content="@solt9029">
-  <meta name="twitter:creator" content="@solt9029">
-  </head>
-  </html>`,
-  help: `
-  <html>
-  <head>
-  <meta name="twitter:card" content="summary">
-  <meta name="twitter:title" content="創作の達人">
-  <meta name="twitter:description" content="創作の達人のヘルプページです。">
-  <meta name="twitter:image" content="http://editmaster.solt9029.com/images/icon.png">
-  <meta name="twitter:url" content="http://editmaster.solt9029.com/help">
-  <meta name="twitter:site" content="@solt9029">
-  <meta name="twitter:creator" content="@solt9029">
-  </head>
-  </html>`,
-  scores: {
-    index: `
-    <html>
-    <head>
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:title" content="創作の達人">
-    <meta name="twitter:description" content="創作の達人の作品一覧ページです。">
-    <meta name="twitter:image" content="http://editmaster.solt9029.com/images/icon.png">
-    <meta name="twitter:url" content="http://editmaster.solt9029.com/scores">
-    <meta name="twitter:site" content="@solt9029">
-    <meta name="twitter:creator" content="@solt9029">
-    </head>
-    </html>`,
-    new: `
-    <html>
-    <head>
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:title" content="創作の達人">
-    <meta name="twitter:description" content="創作の達人の創作ページです。">
-    <meta name="twitter:image" content="http://editmaster.solt9029.com/images/icon.png">
-    <meta name="twitter:url" content="http://editmaster.solt9029.com/scores/new">
-    <meta name="twitter:site" content="@solt9029">
-    <meta name="twitter:creator" content="@solt9029">
-    </head>
-    </html>
-    `,
-    show: score => {
-      return `
-        <html>
-        <head>
-        <meta name="twitter:card" content="summary">
-        <meta name="twitter:title" content="${
-          score.username
-        }さんの創作譜面 #創作の達人">
-        <meta name="twitter:description" content="${score.comment}">
-        <meta name="twitter:image" content="http://i.ytimg.com/vi/${
-          score.video_id
-        }/mqdefault.jpg">
-        <meta name="twitter:url" content="http://editmaster.solt9029.com/scores/${
-          score.id
-        }">
-        <meta name="twitter:site" content="@solt9029">
-        <meta name="twitter:creator" content="@solt9029">
-        </head>
-        </html>
-        `;
-    },
-  },
-};
