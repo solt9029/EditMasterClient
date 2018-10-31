eb1e5f5bf3ccae78ff1e3e2eab2f8cc0a92e9bb6
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-10-31 14:18:08 +0900

    value

--- src/actions/config.test.js
+++ src/actions/config.test.js
@@ -12,12 +12,13 @@ describe('config actions', () => {
   });
 
   it('setUsername action should return required error if the value is empty', () => {
-    const result = config.setUsername('');
+    const value = '';
+    const result = config.setUsername(value);
     const expected = {
       type: actionTypes.CONFIG.SET_USERNAME,
       payload: {
-        value: '',
-        errors: [utils.validations.required('')],
+        value: value,
+        errors: [utils.validations.required(value)],
         touched: true,
       },
     };
@@ -25,13 +26,14 @@ describe('config actions', () => {
   });
 
   it('setUsername action should return maxLength20 error if the length of the value is more than 20', () => {
+    const value = 'aaaaaaaaaaaaaaaaaaaaaaaaa';
     const maxLength20 = utils.validations.maxLength(20);
-    const result = config.setUsername('aaaaaaaaaaaaaaaaaaaaaaaaa');
+    const result = config.setUsername(value);
     const expected = {
       type: actionTypes.CONFIG.SET_USERNAME,
       payload: {
-        value: 'aaaaaaaaaaaaaaaaaaaaaaaaa',
-        errors: [maxLength20('aaaaaaaaaaaaaaaaaaaaaaaaa')],
+        value: value,
+        errors: [maxLength20(value)],
         touched: true,
       },
     };
@@ -39,11 +41,12 @@ describe('config actions', () => {
   });
 
   it('setUsername action should return no errors if the value is not empty and the length of the value is less than 20', () => {
-    const result = config.setUsername('username');
+    const value = 'username';
+    const result = config.setUsername(value);
     const expected = {
       type: actionTypes.CONFIG.SET_USERNAME,
       payload: {
-        value: 'username',
+        value: value,
         errors: [],
         touched: true,
       },
