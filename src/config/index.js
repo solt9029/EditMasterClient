55c87b22685e17e5bc62d609c04aca8ec0befdb9
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-11-08 16:39:49 +0900

    react-ga pageview

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,2 @@
 export { default as api } from './api';
+export { default as googleAnalytics } from './googleAnalytics';
--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './configureStore';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactGA from 'react-ga';
+import { googleAnalytics } from './config';
+
+ReactGA.initialize(googleAnalytics.ID);
+history.listen(({ pathname }) => {
+  ReactGA.set({ page: pathname });
+  ReactGA.pageview(pathname);
+});
 
 const store = configureStore();
 
