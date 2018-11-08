d9f886e3ee3b1da46db8215809c81c2d6da4b4ce
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-11-08 16:39:49 +0900

    react-ga pageview

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import IndexView from './IndexView';
 import ScoresIndexView from '../containers/ScoresIndexView';
 import ScoresNewView from '../containers/ScoresNewView';
@@ -7,20 +7,27 @@ import HelpView from './HelpView';
 import NotFoundView from './NotFoundView';
 import { Switch, Route } from 'react-router-dom';
 import { routes } from '../constants/';
+import ReactGA from 'react-ga';
 
-const App = () => {
-  return (
-    <div>
-      <Switch>
-        <Route exact path={routes.INDEX} component={IndexView} />
-        <Route exact path={routes.SCORES.INDEX} component={ScoresIndexView} />
-        <Route exact path={routes.SCORES.NEW} component={ScoresNewView} />
-        <Route exact path={routes.SCORES.SHOW} component={ScoresShowView} />
-        <Route exact path={routes.HELP} component={HelpView} />
-        <Route component={NotFoundView} />
-      </Switch>
-    </div>
-  );
-};
+export default class App extends Component {
+  componentDidMount() {
+    const { pathname } = this.props.location;
+    ReactGA.set({ page: pathname });
+    ReactGA.pageview(pathname);
+  }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path={routes.INDEX} component={IndexView} />
+          <Route exact path={routes.SCORES.INDEX} component={ScoresIndexView} />
+          <Route exact path={routes.SCORES.NEW} component={ScoresNewView} />
+          <Route exact path={routes.SCORES.SHOW} component={ScoresShowView} />
+          <Route exact path={routes.HELP} component={HelpView} />
+          <Route component={NotFoundView} />
+        </Switch>
+      </div>
+    );
+  }
+}
