c8c31d3c049ec398913dd62ebd172b122c0be1a4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-11-09 18:25:09 +0900

    SFC

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Jumbotron as ReactstrapJumbotron,
   Container,
@@ -44,28 +44,28 @@ const ButtonText = styled.span`
   color: #fff;
 `;
 
-export default class Jumbotron extends Component {
-  render() {
-    return (
-      <StyledJumbotron>
-        <Container>
-          <Title>さあ、創作譜面を始めよう</Title>
-          <Subtitle>
-            <p>
-              誰もが知っている太鼓ゲーム。実は譜面を叩くだけでなく、創作譜面も面白い。
-            </p>
-            <p>
-              "創作の達人"でオリジナルの譜面を創作しよう。誰でも簡単に始められる、創作譜面支援アプリ。
-            </p>
-          </Subtitle>
-          <StyledButton color="primary" size="lg" tag={Link} to="/scores/new">
-            <ButtonText>創作譜面を始める</ButtonText>
-          </StyledButton>
-          <StyledButton color="warning" size="lg" tag={Link} to="/scores">
-            <ButtonText>作品一覧を見る</ButtonText>
-          </StyledButton>
-        </Container>
-      </StyledJumbotron>
-    );
-  }
-}
+const Jumbotron = () => {
+  return (
+    <StyledJumbotron>
+      <Container>
+        <Title>さあ、創作譜面を始めよう</Title>
+        <Subtitle>
+          <p>
+            誰もが知っている太鼓ゲーム。実は譜面を叩くだけでなく、創作譜面も面白い。
+          </p>
+          <p>
+            "創作の達人"でオリジナルの譜面を創作しよう。誰でも簡単に始められる、創作譜面支援アプリ。
+          </p>
+        </Subtitle>
+        <StyledButton color="primary" size="lg" tag={Link} to="/scores/new">
+          <ButtonText>創作譜面を始める</ButtonText>
+        </StyledButton>
+        <StyledButton color="warning" size="lg" tag={Link} to="/scores">
+          <ButtonText>作品一覧を見る</ButtonText>
+        </StyledButton>
+      </Container>
+    </StyledJumbotron>
+  );
+};
+
+export default Jumbotron;
