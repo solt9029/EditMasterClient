3c9253e808a4657defc45a0df30cbd2295f34a8f
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-11-21 17:30:19 +0900

    util

--- src/components/EditorCurrentTimeMarkCanvas.js
+++ src/components/EditorCurrentTimeMarkCanvas.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { numbers, sizes, positions, percentages } from '../constants';
+import { numbers, sizes } from '../constants';
 import Canvas from '../classes/Canvas';
 import * as utils from '../utils';
 
@@ -32,30 +32,17 @@ export default class EditorCurrentTimeMarkCanvas extends Component {
   }
 
   updateCanvas() {
-    this.canvas.clear(
-      this.props.editorPane.width - 1,
-      Math.ceil(this.props.notes.length / numbers.NOTES_PER_BAR) *
-        sizes.EDITOR.BAR.OUTSIDE.HEIGHT
+    const height = utils.calculations.editorCanvasHeight(
+      this.props.notes.length
     );
+    this.canvas.clear(this.props.editorPane.width - 1, height);
 
-    const barWidth =
-      this.props.editorPane.width - 1 - positions.EDITOR.BAR.X * 2;
-    const actualBarWidth = barWidth * (1 - percentages.EDITOR.BAR_START_LINE); // left side of initial beat line is not available
-    const spaceWidth = actualBarWidth / numbers.NOTES_PER_BAR;
-
-    const currentNoteIndexFloat =
-      (this.props.currentTime - this.props.offset) /
-      utils.calculations.secondsPerNote(this.props.bpm);
-    const currentNotesPerBarIndexFloat =
-      currentNoteIndexFloat % numbers.NOTES_PER_BAR;
-    const currentBarIndex = Math.floor(
-      currentNoteIndexFloat / numbers.NOTES_PER_BAR
+    const { x, y } = utils.calculations.currentTimeMark(
+      this.props.editorPane.width,
+      this.props.bpm,
+      this.props.offset,
+      this.props.currentTime
     );
-
-    const y =
-      currentBarIndex * sizes.EDITOR.BAR.OUTSIDE.HEIGHT +
-      (sizes.EDITOR.BAR.OUTSIDE.HEIGHT - sizes.EDITOR.BAR.INSIDE.HEIGHT) / 2;
-    const x = currentNotesPerBarIndexFloat * spaceWidth;
     this.canvas.drawCurrentTimeMark(x, y);
   }
 
