4460d7fb0f7bc6e13b77b4f8edca773f35e2a4a0
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-11-21 11:41:26 +0900

    delete

--- src/containers/IDE.js
+++ src/containers/IDE.js
@@ -1,18 +1,11 @@
 import { connect } from 'react-redux';
 import { setPanes } from '../actions/ide';
-import { setCurrentTime } from '../actions/youtube';
 import IDE from '../components/IDE';
 
-const mapStateToProps = state => ({
-  ytPlayer: state.youtube.ytPlayer,
-  isChangingSlider: state.player.isChangingSlider,
-});
+const mapStateToProps = null;
 const mapDispatchToProps = dispatch => ({
   setPanes(references) {
     dispatch(setPanes(references));
-  },
-  setCurrentTime(currentTime) {
-    dispatch(setCurrentTime(currentTime));
   },
 });
 export default connect(
