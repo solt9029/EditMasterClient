e7892441bbbfa646428d0bfeed0671e26aecf0e5
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-11-24 10:18:35 +0900

    url

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 export { default as notes } from './notes';
 export { default as validations } from './validations';
 export { default as keys } from './keys';
-export { default as urls } from './urls';
+export { default as url } from './url';
 export { default as twitter } from './twitter';
 export { default as calculations } from './calculations';
 export { default as tja } from './tja';
