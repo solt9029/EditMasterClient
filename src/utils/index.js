595f23317b94ad768928a827c0f185ef27d1b281
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-11-24 10:32:43 +0900

    file

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,5 +4,5 @@ export { default as key } from './key';
 export { default as url } from './url';
 export { default as twitter } from './twitter';
 export { default as calculations } from './calculations';
-export { default as tja } from './tja';
+export { default as file } from './file';
 export { default as http } from './http';
