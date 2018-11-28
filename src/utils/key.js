e7cb445fffdc51414294c11281d6b962cf724d81
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-11-29 00:20:05 +0900

    jsdoc

--- src/utils/key.js
+++ src/utils/key.js
@@ -1,9 +1,17 @@
 import { keys } from '../constants';
 
+/**
+ *
+ * @param {string} pushedKey
+ */
 export const isDon = pushedKey => {
   return keys.DON.indexOf(pushedKey) >= 0;
 };
 
+/**
+ *
+ * @param {string} pushedKey
+ */
 export const isKa = pushedKey => {
   return keys.KA.indexOf(pushedKey) >= 0;
 };
--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,17 +1,36 @@
+/**
+ *
+ * @param {string} value
+ * @return {boolean}
+ */
 export const required = value => {
   return value === '' ? '必須です' : false;
 };
 
+/**
+ *
+ * @param {number} max
+ * @return {function}
+ */
 export const maxLength = max => value => {
   return value.length > max
     ? `${max}文字以下の文字列を指定してください`
     : false;
 };
 
+/**
+ *
+ * @param {*} value
+ */
 export const number = value => {
   return isNaN(value) ? '数値を指定してください' : false;
 };
 
+/**
+ *
+ * @param {*} value
+ * @param {*} validations
+ */
 export const validate = (value, validations) => {
   let errors = [];
   validations.forEach(validation => {
