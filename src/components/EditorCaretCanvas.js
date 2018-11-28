5587e0a13fbbaa06a8541a301c71409246085302
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-11-28 22:16:05 +0900

    util canvas

--- src/components/EditorCaretCanvas.js
+++ src/components/EditorCaretCanvas.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { numbers, keys } from '../constants';
-import Canvas from '../classes/Canvas';
-import * as utils from '../utils';
 import Layer from '../styled/Layer';
+import { drawCaret, clear } from '../utils/canvas';
+import { calcCaret, calcEditorCanvasHeight } from '../utils/calculations';
+import { isNote, hasState } from '../utils/note';
 
 export default class EditorCaretCanvas extends Component {
   mouseX = 0;
   mouseY = 0;
   clipboard = null;
   canvasRef = React.createRef();
-  canvas = null;
+  ctx = null;
 
   componentDidMount() {
-    const ctx = this.canvasRef.current.getContext('2d');
-    this.canvas = new Canvas(ctx);
+    this.ctx = this.canvasRef.current.getContext('2d');
   }
 
   keyDown = event => {
@@ -22,7 +22,7 @@ export default class EditorCaretCanvas extends Component {
   };
 
   copyPaste = event => {
-    const { barIndex } = utils.calculations.calcCaret(
+    const { barIndex } = calcCaret(
       this.mouseX,
       this.mouseY,
       this.props.width,
@@ -62,13 +62,13 @@ export default class EditorCaretCanvas extends Component {
     // if the event is key event, the note which is going to be put should be key value!
     if (event.nativeEvent.key) {
       const keyValue = +event.nativeEvent.key;
-      if (!utils.note.isNote(keyValue)) {
+      if (!isNote(keyValue)) {
         return;
       }
       note = keyValue;
     }
 
-    const { barIndex, divisionIndex } = utils.calculations.calcCaret(
+    const { barIndex, divisionIndex } = calcCaret(
       this.mouseX,
       this.mouseY,
       this.props.width,
@@ -79,7 +79,7 @@ export default class EditorCaretCanvas extends Component {
     const mouseNotesPerBarIndex = divisionIndex * notesPerDivision;
     const index = barIndex * numbers.NOTES_PER_BAR + mouseNotesPerBarIndex;
     let notes = [];
-    if (!utils.note.hasState(note)) {
+    if (!hasState(note)) {
       for (let i = 0; i < notesPerDivision; i++) {
         notes.push(note);
       }
@@ -99,23 +99,21 @@ export default class EditorCaretCanvas extends Component {
     this.mouseX = offsetX;
     this.mouseY = offsetY;
 
-    const height = utils.calculations.calcEditorCanvasHeight(
-      this.props.notesLength
-    );
-    this.canvas.clear(this.props.width - 1, height);
+    const height = calcEditorCanvasHeight(this.props.notesLength);
+    clear(this.ctx, this.props.width - 1, height);
 
-    const { x, y } = utils.calculations.calcCaret(
+    const { x, y } = calcCaret(
       this.mouseX,
       this.mouseY,
       this.props.width,
       this.props.palette.division
     );
-    this.canvas.drawCaret(x, y);
+    drawCaret(this.ctx, x, y);
   };
 
   render() {
     const { notesLength, width } = this.props;
-    const height = utils.calculations.calcEditorCanvasHeight(notesLength);
+    const height = calcEditorCanvasHeight(notesLength);
 
     return (
       <Layer
