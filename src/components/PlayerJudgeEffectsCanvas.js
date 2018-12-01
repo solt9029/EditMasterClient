c688592b1bf9afb1e4aa42743f51a8bf7b7b7030
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-12-01 15:28:10 +0900

    judge effects (HACK)

---
+++ src/components/PlayerJudgeEffectsCanvas.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { clear, drawJudgeEffect } from '../utils/canvas';
+import Layer from '../styled/Layer';
+import PropTypes from 'prop-types';
+
+export default class PlayerJudgeEffectsCanvas extends Component {
+  judgeEffects = [];
+  canvasRef = React.createRef();
+  ctx = null;
+
+  componentDidMount() {
+    this.ctx = this.canvasRef.current.getContext('2d');
+    this.updateCanvas();
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    if (
+      this.props.width === nextProps.height &&
+      this.props.height === nextProps.height
+    ) {
+      this.updateCanvas();
+      return false;
+    }
+    return true;
+  }
+
+  updateCanvas() {
+    const { width, height, judgeEffects } = this.props;
+
+    clear(this.ctx, width - 1, height - 1);
+
+    for (let i = judgeEffects.length - 1; i >= 0; i--) {
+      drawJudgeEffect(
+        this.ctx,
+        judgeEffects[i].judgeMarkY,
+        judgeEffects[i].judgeTextY,
+        judgeEffects[i].state
+      );
+    }
+  }
+
+  render() {
+    const { width, height } = this.props;
+
+    return (
+      <Layer innerRef={this.canvasRef} width={width - 1} height={height - 1} />
+    );
+  }
+}
+
+PlayerJudgeEffectsCanvas.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  judgeEffects: PropTypes.arrayOf(PropTypes.object).isRequired,
+  judgeEffectsUpdatedCount: PropTypes.number.isRequired,
+};
