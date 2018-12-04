4bb73255674a7346768c9aa80ea2d7cf11f0b27c
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-12-04 20:06:00 +0900

    default

--- src/reducers/config.js
+++ src/reducers/config.js
@@ -1,13 +1,31 @@
 import { ActionTypes } from '../constants/';
 import { handleActions } from 'redux-actions';
 
-const defaultConfig = {
-  username: '通りすがりの創作の達人',
-  videoId: 'jhOVibLEDhA',
-  bpm: 158,
-  offset: 0.75,
-  comment: '創作の達人で創作譜面をしました！',
-  speed: 1,
+const defaultState = {
+  username: {
+    value: '通りすがりの創作の達人',
+    errors: [],
+  },
+  videoId: {
+    value: 'jhOVibLEDhA',
+    errors: [],
+  },
+  bpm: {
+    value: 158,
+    errors: [],
+  },
+  offset: {
+    value: 0.75,
+    errors: [],
+  },
+  comment: {
+    value: '創作の達人で創作譜面をしました！',
+    errors: [],
+  },
+  speed: {
+    value: 1,
+    errors: [],
+  },
 };
 
 const initialState = {
@@ -39,36 +57,8 @@ const initialState = {
 
 export default handleActions(
   {
-    [ActionTypes.SCORES_NEW_VIEW.SET_DEFAULT_SCORE]: (state, action) => {
-      const { username, videoId, bpm, offset, comment, speed } = defaultConfig;
-      return {
-        ...state,
-        username: {
-          value: username,
-          errors: [],
-        },
-        videoId: {
-          value: videoId,
-          errors: [],
-        },
-        bpm: {
-          value: bpm,
-          errors: [],
-        },
-        offset: {
-          value: offset,
-          errors: [],
-        },
-        comment: {
-          value: comment,
-          errors: [],
-        },
-        speed: {
-          value: speed,
-          errors: [],
-        },
-      };
-    },
+    [ActionTypes.SCORES_NEW_VIEW.SET_DEFAULT_SCORE]: (state, action) =>
+      defaultState,
     [ActionTypes.SCORES_SHOW_VIEW.FINISH_REQUEST_SUCCESS]: (state, action) => {
       const {
         username,
