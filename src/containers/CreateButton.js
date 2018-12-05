8dc491080f0aad74420afb3d76dfcc68036c6471
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-12-05 13:34:50 +0900

    modal

--- src/containers/CreateButton.js
+++ src/containers/CreateButton.js
@@ -1,17 +1,11 @@
 import withRouter from 'react-router-dom/withRouter';
 import { connect } from 'react-redux';
-import { create } from '../actions/modal';
+import { createScore } from '../actions/modal';
 import CreateButton from '../components/CreateButton';
 
-const mapStateToProps = null;
-const mapDispatchToProps = dispatch => ({
-  create() {
-    dispatch(create());
-  },
-});
 export default withRouter(
   connect(
-    mapStateToProps,
-    mapDispatchToProps
+    null,
+    { createScore }
   )(CreateButton)
 );
