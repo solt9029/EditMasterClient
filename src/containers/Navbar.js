961e9308a4eec9bb9073142c9fb9bd3863bde3f3
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-12-08 17:04:43 +0900

    navbar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,10 +1,15 @@
 import withRouter from 'react-router-dom/withRouter';
 import { connect } from 'react-redux';
 import Navbar from '../components/Navbar';
+import { createScore } from '../actions/score';
+import { exportTjaFile } from '../actions/others';
 
 export default withRouter(
-  connect(state => ({
-    isLoading: state.score.fetching.isLoading,
-    error: state.score.fetching.error,
-  }))(Navbar)
+  connect(
+    state => ({
+      isLoading: state.score.fetching.isLoading,
+      error: state.score.fetching.error,
+    }),
+    { createScore, exportTjaFile }
+  )(Navbar)
 );
