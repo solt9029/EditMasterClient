bb1c3eb7a13b24114f25391db9afcf95b347d0c4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-12-08 09:57:21 +0900

    update

--- src/containers/EditorNotesCanvas.js
+++ src/containers/EditorNotesCanvas.js
@@ -5,4 +5,5 @@ export default connect(state => ({
   notes: state.score.notes.list,
   updatedCount: state.score.notes.updatedCount,
   width: state.sizes.editor.width,
+  notesLength: state.score.notes.list.length, // this is necessary for shouldComponentUpdate. (you can't use notes.length instead of notesLength because notes is mutable.)
 }))(EditorNotesCanvas);
