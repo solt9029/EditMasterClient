8b07af5de78202791f7f5952df19f4a2558ac386
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-12-11 20:08:39 +0900

    sfc

--- src/components/ScoreCardPaginate.js
+++ src/components/ScoreCardPaginate.js
@@ -1,49 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactPaginate from 'react-paginate';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { parseSearchQuery, stringifySearchQuery } from '../utils/url';
 
-export default class ScoreCardPaginate extends Component {
-  onPageChange = data => {
-    const { location, history } = this.props;
-
+const ScoreCardPaginate = ({ location, history, currentPage, lastPage }) => {
+  const onPageChange = ({ selected }) => {
     const { keyword } = parseSearchQuery(location.search);
-    const search = stringifySearchQuery(keyword, data.selected + 1);
+    const search = stringifySearchQuery(keyword, selected + 1);
     history.push({
       search,
     });
   };
 
-  render() {
-    const { currentPage, lastPage } = this.props;
+  return (
+    <ReactPaginate
+      class="pagination"
+      previousLabel="前"
+      nextLabel="次"
+      forcePage={currentPage - 1}
+      pageCount={lastPage}
+      marginPagesDisplayed={1}
+      pageRangeDisplayed={3}
+      onPageChange={onPageChange}
+      activeClassName="active"
+      breakClassName="page-item"
+      breakLabel={<a className="page-link">...</a>}
+      pageClassName="page-item"
+      previousClassName="page-item"
+      nextClassName="page-item"
+      pageLinkClassName="page-link"
+      previousLinkClassName="page-link"
+      nextLinkClassName="page-link"
+      containerClassName="pagination justify-content-center"
+    />
+  );
+};
 
-    return (
-      <ReactPaginate
-        class="pagination"
-        previousLabel="前"
-        nextLabel="次"
-        forcePage={currentPage - 1}
-        pageCount={lastPage}
-        marginPagesDisplayed={1}
-        pageRangeDisplayed={3}
-        onPageChange={this.onPageChange}
-        activeClassName="active"
-        breakClassName="page-item"
-        breakLabel={<a className="page-link">...</a>}
-        pageClassName="page-item"
-        previousClassName="page-item"
-        nextClassName="page-item"
-        pageLinkClassName="page-link"
-        previousLinkClassName="page-link"
-        nextLinkClassName="page-link"
-        containerClassName="pagination justify-content-center"
-      />
-    );
-  }
-}
+export default ScoreCardPaginate;
 
 ScoreCardPaginate.propTypes = {
-  lastPage: propTypes.number,
-  currentPage: propTypes.number,
-  resetScores: propTypes.func,
+  lastPage: PropTypes.number,
+  currentPage: PropTypes.number,
 };
