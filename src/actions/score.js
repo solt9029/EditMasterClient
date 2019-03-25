892cb4b8d0fcbeb97f502618697801f02f85a82c
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-03-25 09:56:25 +0900

    delete JSONparse (api changed)

--- src/actions/score.js
+++ src/actions/score.js
@@ -168,7 +168,7 @@ export const fetchScore = id => {
       const result = await _fetchScore(id);
       const { data } = result;
       const videoId = data.video_id;
-      const notes = JSON.parse(data.notes);
+      const notes = data.notes;
       const { username, bpm, offset, speed, comment } = data;
       dispatch(
         finishFetchingScore({
