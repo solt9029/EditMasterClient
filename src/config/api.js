1d8bfc5bb752d4b17531176ba818e6ebf0319f1b
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-06-28 08:08:52 +0900

    env

---
+++ src/config/api.js
@@ -0,0 +1,4 @@
+export default {
+  HOST: process.env.REACT_APP_API_HOST || 'localhost',
+  PORT: process.env.REACT_APP_API_PORT || 8051,
+};
---
+++ src/config/google-analytics.js
@@ -0,0 +1,3 @@
+export default {
+  ID: process.env.REACT_APP_GOOGLE_ANALYTICS_ID || 'UA-128426361-2',
+};
